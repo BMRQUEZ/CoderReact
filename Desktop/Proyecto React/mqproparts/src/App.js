4dc4dd568b762f10673c173aeb04bbaf3a00e3c9
BMRQUEZ <alemarquez1996@gmail.com>
2022-10-10 21:45:40 -0300

    Segunda entrega

--- Desktop/Proyecto React/mqproparts/src/App.js
+++ Desktop/Proyecto React/mqproparts/src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import Navbar from './componentes/Navbar/Navbar';
 import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+ 
 
 function App() {
   return (
-    <>
-     
-     <Navbar />
-     <ItemListContainer greeting={'Nuestros Productos'}/>
-     <ItemDetailContainer /> 
-     
-     </>
+    <div className='App'>
+     <BrowserRouter>
+       <Navbar />
+       <Routes>
+         <Route path= '/' element= {<ItemListContainer greeting={'Nuestros Productos'}/>}/> 
+         <Route path= '/detail/:productId' element={<ItemDetailContainer />}/> 
+         
+       </Routes>
+     </BrowserRouter> 
+     </div> 
   );
 }
 
--- Desktop/Proyecto React/mqproparts/src/componentes/ItemDetailContainer/ItemDetailContainer.js
+++ Desktop/Proyecto React/mqproparts/src/componentes/ItemDetailContainer/ItemDetailContainer.js
@@ -13,7 +13,13 @@ console.log (product)
     return (
         <div>
             <h1>Detalle del producto</h1>
-            
+            <div>
+                <h1>{product?.name} </h1>
+                <h2>{product?.category} </h2>             
+                <h3>{product?.description} </h3>
+                <h3>Stock disponible {product?.stock} </h3>
+        
+            </div>
         </div>
     )
 }
